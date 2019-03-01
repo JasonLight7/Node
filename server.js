f7460f46544be35fd33d5544d8d8e24ffcebc67f
JasonLight7 <47399118+JasonLight7@users.noreply.github.com>
2019-03-01 19:37:39 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const port = process.env.PORT || 8080;
-mongoose.connect('mongodb://localhost:27017/Taquilla');
-//conecta con mongo
-const db = mongoose.connection;
-//almacena la conexion
-db.on('error',console.error.bind(console,'MongoDB connection error'));
-//si hay algun error lo dice
-app.configure(()=>{
-    //configurar express
-   app.use(express.static(__dirname + '/'));
-   app.use(express.logger('dev'));
-   app.use(express.methodOverride());
+//Inicializacion
+var express= require('express');
+var app= express();                    // Utilizamos exress                        
+var mongoose= require('mongoose');		// mongoose para mongodb
+var port= process.env.PORT || 8080;         // Tomamos el puerto 8080
+
+mongoose.connect('mongodb://localhost:27017/CineDesign');  // Hacemos la conexion
+
+var db=mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connetion error:'));
+app.configure(function(){
+app.use(express.static(__dirname + '/'));
+app.use(express.logger('dev'));
+
+app.use(express.methodOverride());
+
+
 });
-const bodyParser = require('body-parser');
-//usa bodyparser
-app.use(bodyParser.json({limit:'50mb'}));
-app.use(bodyParser.urlencoded({limit:'50mb',extended:true}));
-require('./router.js')(app);
-app.listen(port,()=>{console.log(`App por el puerto ${port}`)});
+var bodyParser=require('body-parser');
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+require('./routes.js')(app);
+app.listen(port);
+console.log("APP por el puerto " + port);
+//Poner ; en numeros de calculadora controller//
